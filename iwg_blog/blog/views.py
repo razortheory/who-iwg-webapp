4b81793ebb49e4f468ddd143ca41ee1501988117
Mike <mixser.by@gmail.com>
2016-04-14 11:54:59 +0300

    Added sitelinks search box. Added base view mixin, which add a meta data to the context of view.

--- iwg_blog/blog/views.py
+++ iwg_blog/blog/views.py
@@ -1,36 +1,44 @@
-from django.core.urlresolvers import reverse
 from meta.views import Meta
+
+from django.conf import settings
+from django.contrib.sites.models import Site
+from django.core.urlresolvers import reverse
 
 from django.views.generic import DetailView, ListView
 
 from .models import Article
 
 
-class ArticleView(DetailView):
-    model = Article
-    template_name = 'article.html'
+class BaseViewMixin(object):
+    def get_meta_context(self):
+        raise NotImplementedError
 
     def get_context_data(self, **kwargs):
-        context = super(ArticleView, self).get_context_data(**kwargs)
-        context['meta'] = self.get_object().as_meta(self.request)
+        context = super(BaseViewMixin, self).get_context_data(**kwargs)
+        context['site'] = Site.objects.get_current()
+        context['scheme'] = settings.META_SITE_PROTOCOL
+
+        context['meta'] = self.get_meta_context()
 
         return context
 
 
-class ArticleListView(ListView):
+class ArticleView(BaseViewMixin, DetailView):
+    model = Article
+    template_name = 'article.html'
+
+    def get_meta_context(self):
+        return self.get_object().as_meta(self.request)
+
+
+class ArticleListView(BaseViewMixin, ListView):
     model = Article
     queryset = Article.objects.all()
 
     template_name = 'article_list.html'
 
-    def get_meta_data_for_seo(self):
+    def get_meta_context(self):
         return Meta(title='Article list',
                     description='List of articles.',
                     url=reverse('articles_view')
                     )
-
-    def get_context_data(self, **kwargs):
-        context = super(ArticleListView, self).get_context_data(**kwargs)
-        context['meta'] = self.get_meta_data_for_seo()
-
-        return context
