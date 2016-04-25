38c05b864b4df788da1d08aef39c82c1e29ac770
roman <roman@razortheory.com>
2016-04-25 15:52:34 +0300

    articles autosave feature

--- iwg_blog/blog/forms.py
+++ iwg_blog/blog/forms.py
@@ -11,7 +11,38 @@ def set_attrs_for_field(field, attrs):
     field.widget.attrs.update(attrs)
 
 
-class ArticleAdminForm(forms.ModelForm):
+class AutoSaveModelFormMixin(object):
+    autosave_prefix = ''
+    autosave_fields = []
+
+    def __init__(self, *args, **kwargs):
+        super(AutoSaveModelFormMixin, self).__init__(*args, **kwargs)
+
+        instance = kwargs.get('instance')
+        if instance:
+            instance_identifier = instance.id
+        else:
+            instance_identifier = 'add'
+
+        for field in self.autosave_fields:
+            set_attrs_for_field(
+                self.fields[field],
+                {
+                    'data-autosave': 'autosave_%s_%s:%s' % (self.autosave_prefix, instance_identifier, field),
+                }
+            )
+
+    @property
+    def media(self):
+        media = super(AutoSaveModelFormMixin, self).media
+        media.add_js(['js/models-autosave.js'])
+        return media
+
+
+class ArticleAdminForm(AutoSaveModelFormMixin, forms.ModelForm):
+    autosave_prefix = 'blog_article'
+    autosave_fields = ['content', 'short_description']
+
     def __init__(self, *args, **kwargs):
         super(ArticleAdminForm, self).__init__(*args, **kwargs)
 
