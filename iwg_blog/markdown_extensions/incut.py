a1e61de4bd18de1080b26f63330f31931ca48c5b
Maxim Lomakin <maxim@razortheory.com>
2016-05-04 15:31:07 +0300

    Fix bug in incut markdown extension. Add style for tables and figures.

--- iwg_blog/markdown_extensions/incut.py
+++ iwg_blog/markdown_extensions/incut.py
@@ -5,30 +5,28 @@ from bs4.element import Tag
 
 
 class IncutProcessor(Postprocessor):
-    incut_tags = ['div', 'iframe', 'figure', 'table']
+    incut_tags = ['div', 'iframe', 'figure', 'table', 'pre', 'hr']
     incut_class = 'article__body-incut'
     content_class = 'article__body-content'
 
     def run(self, text):
         soup = BeautifulSoup(text, 'html.parser')
 
-        content = None
+        content = soup.new_tag('div', **{'class': self.content_class})
 
         try:
             tag = soup.children.next()
 
             while tag:
                 if tag.name in self.incut_tags:
-                    soup.append(content)
-                    content = None
+                    if len(content):
+                        soup.append(content)
+                        content = soup.new_tag('div', **{'class': self.content_class})
 
                     incut = soup.new_tag('div', **{'class': self.incut_class})
                     incut.append(tag)
                     soup.append(incut)
                 else:
-                    if not content:
-                        content = soup.new_tag('div', **{'class': self.content_class})
-
                     content.append(tag)
 
                 tag = soup.children.next()
@@ -39,7 +37,7 @@ class IncutProcessor(Postprocessor):
         except StopIteration:
             pass
 
-        if content:
+        if len(content):
             soup.append(content)
 
         return soup.decode()
