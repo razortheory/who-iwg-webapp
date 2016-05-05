d487af8dae6d1af487cb37adee23aad3587c3a87
Maxim Lomakin <maxim@razortheory.com>
2016-05-05 16:00:34 +0300

    Fix incut tag when there are two iframes one by one on the page. Add justify align to article

--- iwg_blog/markdown_extensions/incut.py
+++ iwg_blog/markdown_extensions/incut.py
@@ -1,52 +1,49 @@
 from markdown import Extension
 from markdown.postprocessors import Postprocessor
 from bs4 import BeautifulSoup
-from bs4.element import Tag
+from bs4.element import Tag, NavigableString
 
 
 class IncutProcessor(Postprocessor):
     incut_tags = ['div', 'iframe', 'figure', 'table', 'pre', 'hr']
     incut_class = 'article__body-incut'
+    incut_video_class = 'article__body-incut-video'
     content_class = 'article__body-content'
 
     def run(self, text):
         soup = BeautifulSoup(text, 'html.parser')
+        new_soup = BeautifulSoup()
 
-        content = soup.new_tag('div', **{'class': self.content_class})
+        content = new_soup.new_tag('div', **{'class': self.content_class})
 
-        try:
-            tag = soup.children.next()
+        for tag in soup.children:
+            if tag.name in self.incut_tags:
+                if len(content):
+                    new_soup.append(content)
+                    content = new_soup.new_tag('div', **{'class': self.content_class})
 
-            while tag:
-                if tag.name in self.incut_tags:
-                    if len(content):
-                        soup.append(content)
-                        content = soup.new_tag('div', **{'class': self.content_class})
+                klass = self.incut_class
+                if tag.name == 'iframe':
+                    klass += ' ' + self.incut_video_class
 
-                    incut = soup.new_tag('div', **{'class': self.incut_class})
-                    incut.append(tag)
-                    soup.append(incut)
-                else:
-                    content.append(tag)
-
-                tag = soup.children.next()
-                if isinstance(tag, Tag) and \
-                        (self.incut_class in tag.attrs.get('class', '') or
-                         self.content_class in tag.attrs.get('class', '')):
-                    tag = None
-        except StopIteration:
-            pass
+                incut = soup.new_tag('div', **{'class': klass})
+                incut.append(tag)
+                new_soup.append(incut)
+            elif isinstance(tag, NavigableString):
+                tag.extract()
+            else:
+                content.append(tag)
 
         if len(content):
-            soup.append(content)
+            new_soup.append(content)
 
-        return soup.decode()
+        return new_soup.decode()
 
 
 class IncutExtenssion(Extension):
     def extendMarkdown(self, md, md_globals):
         md.postprocessors.add(
-            'incut', IncutProcessor(md), '_begin',
+            'incut', IncutProcessor(md), '_end',
         )
 
 
