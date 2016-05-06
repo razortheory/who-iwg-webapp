c6276a8a3d413a171b31621c9a0096bc6fa7f5f7
roman <roman@razortheory.com>
2016-05-06 10:57:43 +0300

    fixed top search on index page

--- iwg_blog/blog/static/js/tools.js
+++ iwg_blog/blog/static/js/tools.js
@@ -37,21 +37,56 @@ $('.search-mobile__button').click(function(){
     return false;
 });
 
-var $form = $('.search-top form');
-function onFormFocus(){
-  $(this).addClass('active');
-  $(this).find('input').trigger('input');
-}
-function onFormBlur(){
-  if ($(this).find('input').val().length == 0){
-    $(this).removeClass('active');
+// Search
+$(function () {
+  var $searchInput = $('#id__search-top__field'),
+    $searchResultsWrapper = $('#id__search-top-results'),
+    $searchResults = $('#id__search-top-results__list'),
+    searchItemTemplate = $('#id__search-top-result__template').html();
+
+  $searchInput.on('input', function (e) {
+    var searchTerm = $(this).val();
+    if (searchTerm.length < 3) {
+      $searchResultsWrapper.hide();
+      return;
+    }
+
+    $.get(SEARCH_URL, params = {
+      q: searchTerm,
+    })
+    .done(function (data) {
+      var results = data.data;
+      if (results.length) {
+        $searchResults.empty();
+        for (var i = 0, result; result = results[i]; i++) {
+          var itemHtml = searchItemTemplate.replace(/{\s*([a-zA-Z0-9-_]+)\s*}/g, function (_, key) {
+            return result[key];
+          });
+          $searchResults.append(itemHtml);
+        }
+        $searchResultsWrapper.show();
+      } else {
+        $searchResultsWrapper.hide();
+      }
+    });
+  });
+
+  var $form = $('.search-top form');
+  function onFormFocus(){
+    $(this).addClass('active');
+    $(this).find('input').trigger('input');
   }
-  $(this).find('.search-top-results').hide();
-}
-$form[0].addEventListener('focus', onFormFocus, true);
-$form[0].addEventListener('blur', onFormBlur, true);
-$('.search-top-results, .search-top__button', $form).on('mousedown', function(event){
-  event.preventDefault();
+  function onFormBlur(){
+    if ($(this).find('input').val().length == 0){
+      $(this).removeClass('active');
+    }
+    $(this).find('.search-top-results').hide();
+  }
+  $form[0].addEventListener('focus', onFormFocus, true);
+  $form[0].addEventListener('blur', onFormBlur, true);
+  $('.search-top-results, .search-top__button', $form).on('mousedown', function(event){
+    event.preventDefault();
+  });
 });
 
 function carouFredSelInit (Slider, visible, responsive, width, height, autoplay, circular, align, swipe) {
