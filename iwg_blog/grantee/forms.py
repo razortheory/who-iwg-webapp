27219dd689dc51c21eb3e7b0718dba9e57e543f3
roman <roman@razortheory.com>
2016-05-12 11:41:46 +0300

    fixed overwriting article short description widget from grantee form

--- iwg_blog/grantee/forms.py
+++ iwg_blog/grantee/forms.py
@@ -1,3 +1,5 @@
+import copy
+
 from ..blog.fields import MarkdownFormField
 from ..blog.widgets import CustomMarkdownWidget
 from ..blog.forms import ArticleAdminForm
@@ -5,8 +7,8 @@ from ..blog.forms import ArticleAdminForm
 
 class GranteeAdminForm(ArticleAdminForm):
     class Meta(ArticleAdminForm.Meta):
-        widgets = ArticleAdminForm.Meta.widgets
+        widgets = copy.deepcopy(ArticleAdminForm.Meta.widgets)
         widgets['short_description'] = CustomMarkdownWidget
 
-        field_classes = ArticleAdminForm.Meta.field_classes
+        field_classes = copy.deepcopy(ArticleAdminForm.Meta.field_classes)
         field_classes['short_description'] = MarkdownFormField
