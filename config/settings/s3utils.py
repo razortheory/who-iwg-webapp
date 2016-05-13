a253b8fcd6d4d0e9b92f0f3fef1c335030eb2903
roman <roman@razortheory.com>
2016-05-13 20:47:11 +0300

    added base_url property to static storage

--- config/settings/s3utils.py
+++ config/settings/s3utils.py
@@ -5,7 +5,15 @@ class MediaRootS3BotoStorage(S3BotoStorage):
     location = 'media'
     file_overwrite = False
 
+    @property
+    def base_url(self):
+        return self.url('')
+
 
 class StaticRootS3BotoStorage(S3BotoStorage):
     location = 'static'
     file_overwrite = True
+
+    @property
+    def base_url(self):
+        return self.url('')
