276dab181f4d6125101eb192beac2e2fc5c8d580
roman <roman@razortheory.com>
2016-05-13 19:16:59 +0300

    changed s3 storage from lambda to subclass

--- config/settings/s3utils.py
+++ config/settings/s3utils.py
@@ -1,5 +1,11 @@
 from storages.backends.s3boto import S3BotoStorage
 
 
-MediaRootS3BotoStorage = lambda: S3BotoStorage(location='media', file_overwrite=False)
-StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static', file_overwrite=True)
+class MediaRootS3BotoStorage(S3BotoStorage):
+    location = 'media'
+    file_overwrite = False
+
+
+class StaticRootS3BotoStorage(S3BotoStorage):
+    location = 'static'
+    file_overwrite = True
