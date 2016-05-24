7a947ef4d616234c78399bf29cf400e23b6bdee2
roman <roman@razortheory.com>
2016-05-24 15:07:52 +0300

    moved blog staticfiles to assets folder

--- iwg_blog/thumbnail_lazy/watermarks_config.py
+++ iwg_blog/thumbnail_lazy/watermarks_config.py
@@ -7,7 +7,7 @@ watermark_article = {
     'width': '10%',
     'x': '2%',
     'y': '2%',
-    'url': absolute_url(static('images/who-watermark.png')),
+    'url': absolute_url(static('blog/images/who-watermark.png')),
     'brightness_threshold': 200,
     'color': ['#ffffff', '#9b9b9b'],
     'opacity': 0.6,
