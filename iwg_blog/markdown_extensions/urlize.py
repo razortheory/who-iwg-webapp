ff1eec98faa230460d776eb4efe9ecd917d0c4d3
roman <roman@razortheory.com>
2016-05-24 10:56:10 +0300

    parse emails from article text

--- iwg_blog/markdown_extensions/urlize.py
+++ iwg_blog/markdown_extensions/urlize.py
@@ -14,40 +14,44 @@ from markdown.util import AtomicString, etree
 
 
 class UrlizePattern(Pattern):
-    RE = '(%s)' % '|'.join([
+    RE = '(?P<url>%s)' % '|'.join([
         r'<(?:f|ht)tps?://[^>]*>',
         r'\b(?:f|ht)tps?://[^)<>\s]+[^.,)<>\s]',
         r'\bwww\.[^)<>\s]+[^.,)<>\s]',
         r'[^(<\s]+\.(?:com|net|org)\b',
+        r'\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b'
     ])
 
     def __init__(self, md):
         super(UrlizePattern, self).__init__(self.RE, md)
 
     def handleMatch(self, m):
-        url = m.group(2)
+        url = m.group('url')
 
         if url.startswith('<'):
             url = url[1:-1]
 
         text = url
 
+        el = etree.Element("a")
+        el.text = AtomicString(text)
+
         if not url.split('://')[0] in ('http', 'https', 'ftp'):
             if '@' in url and not '/' in url:
                 url = 'mailto:' + url
             else:
                 url = 'http://' + url
+                el.set('target', '_blank')
 
-        el = etree.Element("a")
         el.set('href', url)
-        el.set('target', '_blank')
-        el.text = AtomicString(text)
         return el
 
 
 class UrlizeExtension(Extension):
     def extendMarkdown(self, md, md_globals):
-        md.inlinePatterns['autolink'] = UrlizePattern(md)
+        md.inlinePatterns.add(
+            'autolink', UrlizePattern(md), '_end',
+        )
 
 
 def makeExtension(*args, **kwargs):
