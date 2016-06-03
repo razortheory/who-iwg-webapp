0c7c9b71e7a1db4e81471c83f9b156777e6e9abc
roman <roman@razortheory.com>
2016-06-03 16:22:51 +0300

    removed unnecessary fields from flatpages, added auto-save to flatpages content

--- iwg_blog/blog/admin.py
+++ iwg_blog/blog/admin.py
@@ -226,7 +226,14 @@ admin.site.unregister(FlatPage)
 @admin.register(FlatPage)
 class FlatPageAdmin(admin.ModelAdmin):
     form = FlatPagesAdminForm
-    change_form_template = 'admin/custom_change_form.html'
+    fields = ['url', 'title', 'content', 'registration_required', ]
+    list_display = ('url', 'title')
+    list_filter = ('sites', 'registration_required')
+    search_fields = ('url', 'title')
+
+    def save_related(self, request, form, formsets, change):
+        super(FlatPageAdmin, self).save_related(request, form, formsets, change)
+        form.instance.sites.add(Site.objects.get_current(request))
 
 
 admin.site.unregister(Site)
