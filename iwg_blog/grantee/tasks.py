39483edf5ca2bc2eceaeb6b0f5bcd8eb28fce465
Maxim Lomakin <maxim@razortheory.com>
2016-06-06 17:25:25 +0300

    Add task for forthcoming posting.

---
+++ iwg_blog/grantee/tasks.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+from django.utils import timezone
+
+from celery.schedules import crontab
+from celery.task import periodic_task
+
+from .models import Grantee
+
+
+@periodic_task(run_every=crontab())
+def publish_articles():
+    Grantee.objects.filter(status=Grantee.STATUS_READY_FOR_PUBLISH, published_at__lte=timezone.now()) \
+        .update(status=Grantee.STATUS_PUBLISHED)
