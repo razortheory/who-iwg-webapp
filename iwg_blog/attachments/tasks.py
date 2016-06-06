64fb1c00588fdf02799aeea756af89cefba08fb5
roman <roman@razortheory.com>
2016-06-06 17:15:55 +0300

    file-based mutex for documents thumbnailer

--- iwg_blog/attachments/tasks.py
+++ iwg_blog/attachments/tasks.py
@@ -1,3 +1,4 @@
+import fcntl
 import os
 
 from django.core.files.base import ContentFile
@@ -12,10 +13,15 @@ def generate_document_preview(document):
     path, filename = os.path.split(document.document_file.name)
     head, ext = os.path.splitext(filename)
 
+    converter_lock = open('converter.lock', 'w+')
+
     try:
+        fcntl.flock(converter_lock, fcntl.LOCK_EX)
         preview_buffer = create_thumbnail(document.document_file, resize_to=400)
     except (ThumbnailerNotFoundException, MimeTypeNotFoundException):
         return
+    finally:
+        fcntl.flock(converter_lock, fcntl.LOCK_UN)
 
     document.file_preview.save(u'%s.jpg' % head, ContentFile(preview_buffer.read()))
 
